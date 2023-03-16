5f8e5917d763068d02a5c8986f4bd34df878f3e3
Evelina <eve1ina@list.ru>
2023-03-17 00:51:24 +0300

    Создан проект

---
+++ app/src/main/java/com/example/pr55/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.pr55;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.customview.widget.Openable;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NavController navController;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, (Openable) null) || super.onSupportNavigateUp();
+    }
+}
